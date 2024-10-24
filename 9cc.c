a1a36ba8fff1cdde21dc8dacfa0f9932d665c2ad
kudos.sakuya.first@gmail.com <kudos.sakuya.first@gmail.com>
2024-10-25 04:26:49 +0900

    引き算と足し算を追加

--- 9cc.c
+++ 9cc.c
@@ -7,10 +7,30 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  char *p = argv[1]; //入力された文字列の先頭の文字を指すポインタ
+
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
-  printf("      mov rax, %d\n", atoi(argv[1]));
+  printf("      mov rax, %ld\n", strtol(p, &p, 10));
+
+  while(*p) { //pが終端文字でない限り動作する
+    if (*p == '+') {
+      p++;
+      printf("      add rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    if (*p == '-') {
+      p++;
+      printf("      sub rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    fprintf(stderr, "予期しない文字です：　'%c'\n", *p);
+    return 1;
+  }
+
   printf("      ret\n");
   return 0;
 }
